4aaea60ae668080705883552331179d161d80dea
Javier Rojas <javierrojas14.jr@gmail.com>
2023-09-27 15:09:17 -0300

    Agrega archivos iniciales de repositorio

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This si the repository to analyze the data of heart transplant done in the Instituto Nacional del Tt Tórax',
+    author='Javier Rojas',
+    license='MIT',
+)
